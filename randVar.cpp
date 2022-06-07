26f20e24903e8dd9ff35413f0407d06b4429ecf4
AjetPi <nikolaa.mihailov@gmail.com>
2022-06-07 16:55:44 +0300

    sth

--- randVar.cpp
+++ randVar.cpp
@@ -11,51 +11,112 @@ using namespace std;
 double EPS = 0.0001;
 
 bool equal(double v1, double v2) {
-    return ((v1 - EPS < v2) && (v1 + EPS > v2));
+  return ((v1 - EPS < v2) && (v1 + EPS > v2));
 }
 
 class RandomVariable {
-    vector<pair<double, double>> values;
-    
+private:
+  vector<pair<double, double>> values;
+  
 public:
-    RandomVariable(vector<pair<double, double>> values = {}) {}
-    
-    double mean() const { return 0; }
-    
-    double variance() const { return 0; }
-    
-    double standardDeviation() const { return 0; }
-    
-    void print() {
-        for(int i = 0; i< values.size(); ++i) {
-            cout<< "("<< values[i].first<< ", "<< values[i].second<< ") ";
-        }
-        cout<< endl;
+  RandomVariable(vector<pair<double, double>> values = {}) {
+    double P = 0;
+    for (int i = 0; i < values.size(); ++i) {
+      P += values[i].second;
     }
+    if (!equal(P, 1.0)) {
+      throw exception();
+    }
+
+    this->values = values;
+  }
+  
+  double mean() const {
+    double m = 0;
+    for (int i = 0; i < values.size(); ++i) {
+      m += values[i].first * values[i].second;
+    }
+
+    return m;
+  }
+  
+  double variance() const {
+    double v = 0;
+    for (int i = 0; i < values.size(); ++i) {
+      v += pow(values[i].first, 2) * values[i].second;
+    }
+
+    return v - mean();
+  }
+  
+  double standardDeviation() const {
+    return sqrt(variance());
+  }
+  
+  void print() {
+    for (int i = 0; i < values.size(); ++i) {
+      cout << "(" << values[i].first << ", " << values[i].second << ") ";
+    }
+    cout << endl;
+  }
 };
 
-double binomialDensity(int n, int k, double p) { return 0; }
+int factorial(int n) {
+  int f = 1;
+  for (int i = n; i > 1; --i) {
+    f *= i;
+  }
 
-RandomVariable binomialRandomVariable(int n, double p) { return RandomVariable(); }
+  return f;
+}
 
-double binomialRandom(int n, double p) { return 0; }
+double binomialDensity(int n, int k, double p) {
+  return (factorial(n) / (factorial(k) * factorial(n - k))) * pow(p, k) * pow((1 - p), (n - k));
+}
+
+RandomVariable binomialRandomVariable(int n, double p) {
+  vector<pair<double, double>> values;
+  for (int i = 0; i <= n; ++i) {
+    values.push_back(make_pair(i, binomialDensity(n, i, p)));
+  }
+
+  return RandomVariable(values);
+}
+
+double binomialRandom(int n, double p) {
+  int k = 0;
+  int t = 1.0 / p;
+  for (int i = 0; i < n; ++i) {
+    if (!(rand() % t)) {
+      ++k;
+    }
+  }
+
+  return k;
+}
 
 int main() {
-    std::srand(std::time(nullptr));
-    
-    RandomVariable rv(vector<pair<double, double>>({ 
-        make_pair(0, 0.5), 
-        make_pair(1, 0.5) 
-    }));
-    rv.print();
-    
-    cout<< rv.mean()<< " "<< rv.variance()<< endl;
-    cout<< binomialDensity(3, 2, 0.5)<< endl;
-    
-    RandomVariable b(binomialRandomVariable(3, 0.5));
-    b.print();
-    
-    cout<< binomialRandom(200, 0.5);
+  srand(time(nullptr));
+  
+  RandomVariable rv(vector<pair<double, double>>({ 
+    make_pair(0, 0.5), 
+    make_pair(1, 0.5)
+  }));
+  rv.print();
+  
+  cout << rv.mean() << " " << rv.variance() << endl;
+  cout << binomialDensity(3, 2, 0.5) << endl;
+  
+  RandomVariable b(binomialRandomVariable(3, 0.5));
+  b.print();
+  
+  /*
+  // 7.
+  cout << binomialDensity(234, 40, 1.0 / 6.0) << endl;
+  cout << binomialDensity(234, 40, 1.0 / 6.0) + binomialDensity(234, 39, 1.0 / 6.0) + [...] + binomialDensity(234, 1, 1.0 / 6.0) << endl;
+  */
 
-    return 0;
+  cout << binomialRandom(200, 0.5) << endl;
+
+  return 0;
 }
